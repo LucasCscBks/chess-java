9b2427798421539314e3dfb2394a9d5417863b16
LucasCscBks <luke100191@gmail.com>
2024-01-19 16:21:31 -0300

    new class Board and Piece

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Board;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        Board board = new Board(8,8);
+        
+    }
+}
