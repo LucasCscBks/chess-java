974f691efc6e2a4738fe8d8cef3ae9ca5a0dd7e0
LucasCscBks <luke100191@gmail.com>
2024-01-19 22:49:45 -0300

    possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
     
@@ -13,15 +13,21 @@ public class Piece {
         return board;
     }
 
-    // public Boolean[][] possibleMoves() {
+    public abstract boolean[][] possibleMoves();
 
-    // }
+    public Boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
 
-    // public Boolean possibleMove(Position position) {
-
-    // }
-
-    // public Boolean isThereAnyPossibleMove() {
-
-    // }
+    public Boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
