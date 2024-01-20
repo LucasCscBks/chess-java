59540ad372e5668287c98660557d9e411395a16c
LucasCscBks <luke100191@gmail.com>
2024-01-19 23:18:57 -0300

    implementation rook movements

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -14,8 +14,16 @@ public class Position {
     public Integer getRow() {
         return row;
     }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
     public Integer getColumn() {
         return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
     }
 
     public void setValues(Integer row, Integer column) {
